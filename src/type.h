139fde811a144836e37cd34fd8e6ae29118193d8
Alexandru <alexandru.vlaicu@gmail.com>
2025-05-20 13:41:50 +0200

    ajout de type.h

---
+++ src/type.h
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#ifndef TYPE_H
+#define TYPE_H
+
+typedef size_t sommet;
+typedef uint8_t Mac[6];
+typedef size_t IP[4];
+
+typedef struct {
+    int16_t port;
+    Mac destination;
+} commutation;
+
+
+typedef struct {
+    Mac addrMac;
+    IP addrIP;
+    uint16_t nbPort;
+    uint16_t priorite;
+    commutation* commutations;
+    uint8_t type;
+}equipement;
+
+typedef enum {
+    ICMP,BPDU,ARP,IPv4,IPv6,DHCP
+}typeTrame;
+
+typedef struct {
+    Mac addrDestination;
+    Mac addrSource;
+    typeTrame type;
+}trame;
+
+#endif
