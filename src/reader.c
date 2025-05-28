80e03ec6f238d0fb238bc11821af3b0d885b5667
aymeric <aymeric.fenard@etu.unistra.fr>
2025-05-28 17:31:56 +0200

    Affichage réussi avec la fonction readFile

--- src/reader.c
+++ src/reader.c
@@ -10,26 +10,24 @@
 #define STR_MAC_LENGTH 18
 #define STR_IP_LENGTH 16
 
-
+// Fonction pour lire le fichier et afficher les informations
 void readFile(char* fileName, char* fileString) {
     
     FILE* inputFile = fopen(fileName, "r");
 
+    Mac* adresseMac = NULL;
+    adresseMac = malloc(sizeof(Mac));
+    IP* adresseIP = NULL;
+    adresseIP = malloc(sizeof(IP));
+
     int nbSommets = 0;
     int nbAretes = 0;
     int typeMachine = 0;
-    char* adresseMac = NULL;
     int port = 0;
     int priorite = 0;
-
-    char* adresseIP = NULL;
-
     int s1 = 0;
     int s2 = 0;
     int poids = 0;
-
-    adresseMac = malloc(STR_MAC_LENGTH);
-    adresseIP = malloc(STR_IP_LENGTH);
 
     if(inputFile == NULL){
         printf("Impossible d'ouvrir le fichier %s\n", fileName);
@@ -54,17 +52,39 @@ void readFile(char* fileName, char* fileString) {
             exit(-1);
         }
         if(fileString[0] == '2'){
-            sscanf(fileString, "%d;%17s;%d;%d", &typeMachine, adresseMac, &port, &priorite);
-            printf("Type de machine : %d\n", typeMachine);
-            printf("Adresse MAC : %s\n", adresseMac);
-            printf("Port : %d\n", port);
+            sscanf(fileString, "%d;%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx;%d;%d", &typeMachine, *adresseMac, *(adresseMac+1), *(adresseMac+2), *(adresseMac+3), *(adresseMac+4), *(adresseMac+5), &port, &priorite);
+            printf("Type de machine : %d\t", typeMachine);
+            printf("Adresse MAC : ");
+            for (int h = 0; h < 6; h++)
+            {
+                printf("%02x", **(adresseMac+h));
+                if (h < 5) {
+                    printf(":");
+                }
+            }
+            printf("\tPort : %d\t", port);
             printf("Priorité : %d\n", priorite);
+            printf("\n");
         }
         else{
-            sscanf(fileString, "%d;%17s;%15s", &typeMachine, adresseMac, adresseIP);
-            printf("Type de machine : %d\n", typeMachine);
-            printf("Adresse MAC : %s\n", adresseMac);
-            printf("Adresse IP : %s\n", adresseIP);
+            sscanf(fileString, "%d;%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx;%hhu.%hhu.%hhu.%hhu", &typeMachine, *adresseMac, *(adresseMac+1), *(adresseMac+2), *(adresseMac+3), *(adresseMac+4), *(adresseMac+5), *adresseIP, *(adresseIP+1), *(adresseIP+2), *(adresseIP+3));
+            printf("Type de machine : %d\t", typeMachine);
+            printf("Adresse MAC : ");
+            for (int h = 0; h < 6; h++)
+            {
+                printf("%02x", **(adresseMac+h));
+                if (h < 5) {
+                    printf(":");
+                }
+            }
+            printf("\tAdresse IP : ");
+            for (int j = 0; j < 4; j++) {
+                printf("%d", **(adresseIP+j));
+                if (j < 3) {
+                    printf(".");
+                }
+            }
+            printf("\n\n");
         }
     }
 
@@ -76,17 +96,13 @@ void readFile(char* fileName, char* fileString) {
             exit(-1);
         }
         sscanf(fileString, "%d;%d;%d", &s1, &s2, &poids);
-        printf("Sommets : s1 : %d | s2 : %d\n", s1, s2);
+        printf("Arete : s1 : %d\t| s2 : %d\t", s1, s2);
         printf("Poids : %d\n", poids);
     }
 
     fclose(inputFile);
     free(fileString);
     fileString = NULL;
-    free(adresseMac);
-    adresseMac = NULL;
-    free(adresseIP);
-    adresseIP = NULL;
     printf("Le fichier a été traité avec succès\n");
     exit(0);
 }
