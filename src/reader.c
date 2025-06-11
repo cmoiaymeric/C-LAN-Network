caaa8ba6ee14187f2b6eb10a42d0046da47eadec
Alexandru <alexandru.vlaicu@gmail.com>
2025-06-11 14:29:55 +0200

    ajout des comms sur reader c et h

--- src/reader.c
+++ src/reader.c
@@ -12,7 +12,15 @@
 
 #define MAX_STR_LENGTH 128
 
-// Fonction pour lire le fichier et afficher les informations
+/**
+ * @brief Lit un fichier de configuration et remplit la structure réseau.
+ * 
+ * Lit le nombre de machines et connexions, puis initialise chaque machine
+ * (station ou switch) en fonction des données, et ajoute les connexions.
+ * 
+ * @param file_name Nom du fichier de configuration à lire
+ * @param reseau Pointeur vers la structure réseau à remplir
+ */
 void read_config_file(char* file_name, Reseau* reseau) {
 
     char * file_string = NULL;
@@ -20,25 +28,26 @@ void read_config_file(char* file_name, Reseau* reseau) {
     FILE* input_file = fopen(file_name, "r");
 
     Mac* adresse_mac = NULL;
-    adresse_mac = malloc(sizeof(Mac));
+    adresse_mac = malloc(sizeof(Mac)); // Pour stocker temporairement une adresse MAC lue
     IP* adresse_ip = NULL;
-    adresse_ip = malloc(sizeof(IP));
+    adresse_ip = malloc(sizeof(IP)); // Pour stocker temporairement une adresse IP lue
 
-    uint8_t nb_sommets = 0;
-    uint8_t nb_aretes = 0;
-    uint8_t type_machine = 0;
-    uint8_t ports = 0;
-    uint16_t priorite = 0;
-    uint16_t s1 = 0;
-    uint16_t s2 = 0;
-    uint16_t poids = 0;
+    uint8_t nb_sommets = 0;   // Nombre de machines à lire
+    uint8_t nb_aretes = 0;    // Nombre de connexions à lire
+    uint8_t type_machine = 0; // Type de machine (switch ou station)
+    uint8_t ports = 0;        // Nombre de ports (pour un switch)
+    uint16_t priorite = 0;    // Priorité (pour un switch)
+    uint16_t s1 = 0;          // Sommet 1 d'une connexion
+    uint16_t s2 = 0;          // Sommet 2 d'une connexion
+    uint16_t poids = 0;       // Poids de la connexion
 
     if(input_file == NULL){
         printf("Impossible d'ouvrir le fichier %s\n", file_name);
         exit(-1);
     }
 
-    file_string = (char*) malloc(MAX_STR_LENGTH);
+    file_string = (char*) malloc(MAX_STR_LENGTH);  // Buffer pour lire chaque ligne
+    // Lecture de la première ligne contenant nb_sommets et nb_aretes
     fgets(file_string, MAX_STR_LENGTH, input_file);
 
     //Traitement d'une erreur de lecture
@@ -67,12 +76,14 @@ void read_config_file(char* file_name, Reseau* reseau) {
         if(type_machine == TypeSwitch){
 
             //Lit la ligne pour la configuration d'un switch
+            // type_machine;MAC;nb_ports;priorite
             sscanf(file_string, "%hhd;%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx;%hhd;%hd", &type_machine, &adresse_mac->octets[0], &adresse_mac->octets[1], &adresse_mac->octets[2], &adresse_mac->octets[3], &adresse_mac->octets[4], &adresse_mac->octets[5], &ports, &priorite);
             
             init_switch(machine, *adresse_mac, ports, priorite);
         }
         else{
             //Lit la ligne pour la configuration d'une station
+            // type_machine;MAC;IP
             sscanf(file_string, "%hhd;%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx;%hhu.%hhu.%hhu.%hhu", &type_machine, &adresse_mac->octets[0], &adresse_mac->octets[1], &adresse_mac->octets[2], &adresse_mac->octets[3], &adresse_mac->octets[4], &adresse_mac->octets[5], &adresse_ip->octets[0], &adresse_ip->octets[1], &adresse_ip->octets[2], &adresse_ip->octets[3]);
            
             init_station(machine, *adresse_mac, *adresse_ip);
@@ -81,6 +92,7 @@ void read_config_file(char* file_name, Reseau* reseau) {
         free(machine);
     }
 
+ // Lecture des connexions (arêtes)
     for(uint8_t i = 0; i < nb_aretes; i++) {
         fgets(file_string, MAX_STR_LENGTH, input_file);
         
@@ -90,6 +102,7 @@ void read_config_file(char* file_name, Reseau* reseau) {
             break;
             exit(-1);
         }
+        // Lit les deux sommets et le poids de la connexion
         sscanf(file_string, "%hd;%hd;%hd", &s1, &s2, &poids);
 
         ajouter_connection(reseau, (Connexion){s1,s2,poids});
--- src/reader.h
+++ src/reader.h
@@ -2,5 +2,14 @@
 
 #include <stdint.h>
 #include "reseau.h"
-
+/**
+ * @brief Lit un fichier de configuration pour initialiser un réseau.
+ * 
+ * Cette fonction ouvre le fichier spécifié par file_name, lit les informations
+ * sur les machines (stations et switches) ainsi que les connexions, puis
+ * remplit la structure Reseau passée en paramètre.
+ * 
+ * @param file_name Nom du fichier de configuration à lire
+ * @param reseau Pointeur vers la structure Reseau à initialiser
+ */
 void read_config_file(char* file_name, Reseau* reseau);
