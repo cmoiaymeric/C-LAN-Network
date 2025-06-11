50a9f1e06c9cdc5c0b669429a58d34f81f7b4765
Alexandru <alexandru.vlaicu@gmail.com>
2025-06-11 15:21:42 +0200

    ajout de comms trame c et type h

--- src/trame.c
+++ src/trame.c
@@ -1,30 +1,50 @@
 #include "trame.h"
 
+/**
+ * @brief Initialise une trame Ethernet avec les informations fournies.
+ * 
+ * @param trame        Pointeur vers la structure Trame à initialiser.
+ * @param sourceMac    Adresse MAC source.
+ * @param destMac      Adresse MAC destination.
+ * @param type         Type de la trame (valeur personnalisée ou type Ethernet).
+ * @param data         Contenu des données (payload) à insérer dans la trame.
+ */
 void init_trame(Trame *trame, Mac* sourceMac, Mac* destMac, uint16_t type, char *data)
 {
-    trame->SFD = 171;
+    trame->SFD = 171; // Start Frame Delimiter : valeur fixe indiquant le début de la trame
 
+    // Copie des octets de l'adresse MAC source
     for (uint8_t i = 0; i < 6; i++)
     {
         trame->addrSource.octets[i] = sourceMac->octets[i];
     }
 
+    // Copie des octets de l'adresse MAC destination
     for (uint8_t i = 0; i < 6; i++)
     {
         trame->addrDestination.octets[i] = destMac->octets[i];
     }
 
+    // Affectation du type de trame
     trame->type = type;
 
+    // Copie des données caractère par caractère
     for (int i = 0; i < strlen(data); i++)
     {
         trame->data[i] = data[i];
     }
 
+    // Ajout du caractère nul à la fin de la chaîne
     trame->data[strlen(data)] = '\0';
+    // FCS (Frame Check Sequence) à 0 (non calculé ici)
     trame->FCS = 0;
 }
 
+/**
+ * @brief Réinitialise les champs d'une trame à 0 et libère la mémoire si nécessaire.
+ * 
+ * @param trame   Pointeur vers la structure Trame à désinitialiser.
+ */
 void deinit_trame(Trame *trame)
 {
     trame->SFD = 0;
--- src/type.h
+++ src/type.h
@@ -7,22 +7,69 @@
 #ifndef TYPE_H
 #define TYPE_H
 
+
+// Définition d'un alias pour un octet
 typedef uint8_t octet;
 
+/**
+ * @brief Représente une adresse MAC.
+ * Composée de 6 octets.
+ */
 typedef struct Mac {
     octet octets[6];
 }Mac;
 
+
+/**
+ * @brief Représente une adresse IP.
+ * Composée de 4 octets.
+ */
 typedef struct IP {
     octet octets[4];
 }IP;
 
+/**
+ * @brief Convertit une adresse MAC en chaîne de caractères (format : xx:xx:xx:xx:xx:xx).
+ * 
+ * @param adresse_mac Adresse MAC à convertir.
+ * @param mac_string Chaîne de caractères préallouée (au moins 18 caractères).
+ * @return Pointeur vers la chaîne formatée.
+ */
 char * mac_to_string(Mac adresse_mac, char * mac_string);
+/**
+ * @brief Convertit une adresse IP en chaîne de caractères (format : xxx.xxx.xxx.xxx).
+ * 
+ * @param adresse_ip Adresse IP à convertir.
+ * @param ip_string Chaîne de caractères préallouée (au moins 16 caractères).
+ * @return Pointeur vers la chaîne formatée.
+ */
 char * ip_to_string(IP adresse_ip, char * ip_string);
 
+/**
+ * @brief Transforme une chaîne de caractères en adresse MAC.
+ * 
+ * @param mac_string Chaîne au format "xx:xx:xx:xx:xx:xx".
+ * @param adresse_mac Pointeur vers la structure MAC à remplir.
+ * @return Pointeur vers la structure remplie.
+ */
 Mac* mac_from_string(char * mac_string, Mac* adresse_mac);
+
+/**
+ * @brief Transforme une chaîne de caractères en adresse IP.
+ * 
+ * @param ip_string Chaîne au format "xxx.xxx.xxx.xxx".
+ * @param adresse_ip Pointeur vers la structure IP à remplir.
+ * @return Pointeur vers la structure remplie.
+ */
 IP* ip_from_string(char * ip_string, IP* adresse_ip);
 
+/**
+ * @brief Compare deux adresses MAC.
+ * 
+ * @param addr1 Première adresse MAC.
+ * @param addr2 Deuxième adresse MAC.
+ * @return 1 si les adresses sont identiques, 0 sinon.
+ */
 int comparer_adresses_mac( Mac addr1, Mac addr2);
 
 #endif
